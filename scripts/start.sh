e5119a325da7642b35762754a7b7009061ddbef0
DoubleMalt <christoph@web.crofting.com>
2014-10-09 20:59:27 +0200

    fix name of environment variable

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-/Mailpile/mp set sys.subdirectory = "/mailpile/${CLOUDFLEET_USER}"
+/Mailpile/mp set sys.subdirectory = "/mailpile/${CLOUDFLEET_USERNAME}"
 /Mailpile/mp --www=0.0.0.0:33411 --wait
