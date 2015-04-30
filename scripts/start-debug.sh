f757c05938d6d0c209b85a6d40417e42b7460668
Christoph Witzany <christoph@web.crofting.com>
2015-04-30 19:20:09 +0200

    change name of sys config variable for path

--- scripts/start-debug.sh
+++ scripts/start-debug.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-/Mailpile/mp set sys.subdirectory = "/mailpile/${CLOUDFLEET_USERNAME}"
-/Mailpile/mp set sys.debug = log http 
+/Mailpile/mp set sys.http_path = "/mailpile/${CLOUDFLEET_USERNAME}"
+/Mailpile/mp set sys.debug = log http
 /Mailpile/mp --www=0.0.0.0:33411 --wait
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-/Mailpile/mp set sys.subdirectory = "/mailpile/${CLOUDFLEET_USERNAME}"
+/Mailpile/mp set sys.http_path = "/mailpile/${CLOUDFLEET_USERNAME}"
 /Mailpile/mp --www=0.0.0.0:33411 --wait
