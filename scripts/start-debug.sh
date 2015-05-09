f20366dc3cbf3577c741fbda4b71c8a1c1fe9434
Christoph Witzany <christoph@web.crofting.com>
2015-05-09 21:39:04 +0200

    fix also start-debug.sh

--- scripts/start-debug.sh
+++ scripts/start-debug.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 /sbin/ip route|awk '/default/ { print  $3,"\tblimp-docker-host" }' >> /etc/hosts
 /sbin/ip route|awk '/default/ { print  $3,"\tblimp-docker" }' >> /etc/hosts
-/Mailpile/mp set sys.http_path = "/mailpile/${CLOUDFLEET_USERNAME}"
 /Mailpile/mp set sys.debug = log http
-/Mailpile/mp --www=0.0.0.0:33411 --wait
+/Mailpile/mp --www=0.0.0.0:33411/mailpile/${CLOUDFLEET_USERNAME}/ --wait
