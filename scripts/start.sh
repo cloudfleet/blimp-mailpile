7b44651a5ea9ca958e95e5808d0e9f29b8c03468
Christoph Witzany <christoph@web.crofting.com>
2015-05-09 20:56:26 +0200

    simplify startscript

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 /sbin/ip route|awk '/default/ { print  $3,"\tblimp-docker-host" }' >> /etc/hosts
 /sbin/ip route|awk '/default/ { print  $3,"\tblimp-docker" }' >> /etc/hosts
-/Mailpile/mp set sys.http_path = "/mailpile/${CLOUDFLEET_USERNAME}"
-/Mailpile/mp --www=0.0.0.0:33411 --wait
+/Mailpile/mp --www=0.0.0.0:33411/mailpile/${CLOUDFLEET_USERNAME}/ --wait
