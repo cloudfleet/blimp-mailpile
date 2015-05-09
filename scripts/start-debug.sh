227863b7a8079e18302493935f08a8bf761cb9f2
Christoph Witzany <christoph@web.crofting.com>
2015-05-09 21:52:11 +0200

    remove trailing slashes

--- scripts/start-debug.sh
+++ scripts/start-debug.sh
@@ -2,4 +2,4 @@
 /sbin/ip route|awk '/default/ { print  $3,"\tblimp-docker-host" }' >> /etc/hosts
 /sbin/ip route|awk '/default/ { print  $3,"\tblimp-docker" }' >> /etc/hosts
 /Mailpile/mp set sys.debug = log http
-/Mailpile/mp --www=0.0.0.0:33411/mailpile/${CLOUDFLEET_USERNAME}/ --wait
+/Mailpile/mp --www=0.0.0.0:33411/mailpile/${CLOUDFLEET_USERNAME} --wait
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 /sbin/ip route|awk '/default/ { print  $3,"\tblimp-docker-host" }' >> /etc/hosts
 /sbin/ip route|awk '/default/ { print  $3,"\tblimp-docker" }' >> /etc/hosts
-/Mailpile/mp --www=0.0.0.0:33411/mailpile/${CLOUDFLEET_USERNAME}/ --wait
+/Mailpile/mp --www=0.0.0.0:33411/mailpile/${CLOUDFLEET_USERNAME} --wait
