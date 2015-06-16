8f65f36ac9df64b16f64fdb47f05a08571289f90
Christoph Witzany <christoph@web.crofting.com>
2015-06-16 19:16:32 +0200

    Drop privileges

--- scripts/start-debug.sh
+++ scripts/start-debug.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
+chown -r mailpile:mailpile /mailpile-data/.local/share/Mailpile
+chown -r mailpile:mailpile /mailpile-data/.gnupg
+
+su - mailpile
+
+/Mailpile/mp setup
 /Mailpile/mp set sys.debug = log http
 /Mailpile/mp --www=0.0.0.0:33411/mailpile/${CLOUDFLEET_USERNAME} --wait
