3673bdb566494bc3af81a7d9d884850cb2bcc6d9
Christoph Witzany <christoph@web.crofting.com>
2015-06-16 16:05:50 +0200

    remove editing of /etc/hosts

--- scripts/start-debug.sh
+++ scripts/start-debug.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
-/sbin/ip route|awk '/default/ { print  $3,"\tblimp-docker-host" }' >> /etc/hosts
-/sbin/ip route|awk '/default/ { print  $3,"\tblimp-docker" }' >> /etc/hosts
 /Mailpile/mp set sys.debug = log http
 /Mailpile/mp --www=0.0.0.0:33411/mailpile/${CLOUDFLEET_USERNAME} --wait
