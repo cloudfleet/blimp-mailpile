12c335911d6e2cc9d5b025a4eafe09c438ade846
Christoph Witzany <christoph@web.crofting.com>
2015-06-23 16:54:24 +0200

    fix startup of mailpile

--- scripts/start-debug.sh
+++ scripts/start-debug.sh
@@ -2,8 +2,10 @@
 chown -r mailpile:mailpile /mailpile-data/.local/share/Mailpile
 chown -r mailpile:mailpile /mailpile-data/.gnupg
 
-su - mailpile
+su - mailpile <<EOF
 
 /Mailpile/mp setup
 /Mailpile/mp set sys.debug = log http
 /Mailpile/mp --www=0.0.0.0:33411/mailpile/${CLOUDFLEET_USERNAME} --wait
+
+EOF
