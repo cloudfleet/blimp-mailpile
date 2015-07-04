8755c29c296ed44577adea73f639579783781767
Christoph Witzany <christoph@web.crofting.com>
2015-07-04 09:39:31 +0200

    make sure directories are created and stop if anything goes wrong

--- scripts/start-debug.sh
+++ scripts/start-debug.sh
@@ -1,10 +1,16 @@
 #! /bin/bash
-chown -R mailpile:mailpile /mailpile-data/.local/share/Mailpile
-chown -R mailpile:mailpile /mailpile-data/.gnupg
+set -e
+
+mkdir -p /mailpile-data/.local/share/Mailpile/default
+mkdir -p /mailpile-data/.gnupg
 
 if [ ! -f /mailpile-data/.local/share/Mailpile/default/mailpile.cfg ]; then
+  echo "Copying initial Mailpile config"
   cp /opt/cloudfleet/setup/scripts/mailpile.cfg /mailpile-data/.local/share/Mailpile/default/mailpile.cfg
 fi
+
+chown -R mailpile:mailpile /mailpile-data/.local/share/Mailpile
+chown -R mailpile:mailpile /mailpile-data/.gnupg
 
 su - mailpile <<EOF
 
