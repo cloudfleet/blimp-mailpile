6bfb4c32613f048bf33bf59cdc93cf8a810a65e2
Christoph Witzany <christoph@web.crofting.com>
2015-07-04 10:00:13 +0200

    add debugging

--- scripts/start-debug.sh
+++ scripts/start-debug.sh
@@ -9,12 +9,16 @@ if [ ! -f /mailpile-data/.local/share/Mailpile/default/mailpile.cfg ]; then
   cp /opt/cloudfleet/setup/scripts/mailpile.cfg /mailpile-data/.local/share/Mailpile/default/mailpile.cfg
 fi
 
+wc -l /mailpile-data/.local/share/Mailpile/default/mailpile.cfg
+
 chown -R mailpile:mailpile /mailpile-data/.local/share/Mailpile
 chown -R mailpile:mailpile /mailpile-data/.gnupg
 
 su - mailpile <<EOF
-
+wc -l /mailpile-data/.local/share/Mailpile/default/mailpile.cfg
 /Mailpile/mp set sys.debug = log http
+wc -l /mailpile-data/.local/share/Mailpile/default/mailpile.cfg
 /Mailpile/mp --www=0.0.0.0:33411/mailpile/${CLOUDFLEET_USERNAME} --wait
+wc -l /mailpile-data/.local/share/Mailpile/default/mailpile.cfg
 
 EOF
