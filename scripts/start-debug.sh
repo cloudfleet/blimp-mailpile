05eedf464cabd1ba53122be124bad6cd4bd348d5
Christoph Witzany <christoph@web.crofting.com>
2015-07-05 21:40:58 +0200

    add mailpile.rc file

--- scripts/start-debug.sh
+++ scripts/start-debug.sh
@@ -2,11 +2,10 @@
 set -e
 
 mkdir -p /mailpile-data/.local/share/Mailpile/default
-mkdir -p /mailpile-data/.gnupg
 
 if [ ! -f /mailpile-data/.local/share/Mailpile/default/mailpile.cfg ]; then
   echo "Copying initial Mailpile config"
-  cp /opt/cloudfleet/setup/scripts/mailpile.cfg /mailpile-data/.local/share/Mailpile/default/mailpile.cfg
+  cp /opt/cloudfleet/setup/scripts/mailpile.* /mailpile-data/.local/share/Mailpile/default/
 fi
 
 wc -l /mailpile-data/.local/share/Mailpile/default/mailpile.cfg
