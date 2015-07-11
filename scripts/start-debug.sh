b2755e93a0afda246f49ed068f8805d48a0bf67b
Christoph Witzany <christoph@web.crofting.com>
2015-07-11 19:21:46 +0200

    create initial profile

--- scripts/start-debug.sh
+++ scripts/start-debug.sh
@@ -8,10 +8,12 @@ if [ ! -f /mailpile-data/.local/share/Mailpile/default/mailpile.cfg ]; then
   cp /opt/cloudfleet/setup/scripts/mailpile.* /mailpile-data/.local/share/Mailpile/default/
 fi
 
-wc -l /mailpile-data/.local/share/Mailpile/default/mailpile.cfg
-
-chown -R mailpile:mailpile /mailpile-data/.local/share/Mailpile
-chown -R mailpile:mailpile /mailpile-data/.gnupg
+if [ ! -f /mailpile-data/.local/share/Mailpile/default/cloudfleet.vcf ]; then
+  echo "Copying initial Mailpile profile"
+  EOF=EOF_$RANDOM; eval echo "\"$(cat <<$EOF
+  $(< scripts/template-main.vcf) $EOF
+  )\""
+fi
 
 su - mailpile <<EOF
 /Mailpile/mp --www=0.0.0.0:33411/mailpile/${CLOUDFLEET_USERNAME} --wait
