08e4bbd786210b3764aeda093e759261f80fbbec
Christoph Witzany <christoph@web.crofting.com>
2015-07-11 21:29:37 +0200

    readd chown

--- scripts/start-debug.sh
+++ scripts/start-debug.sh
@@ -29,6 +29,9 @@ X-MAILPILE-RID:cloudfleet
 EOF
 fi
 
+chown -R mailpile:mailpile /mailpile-data/.local/share/Mailpile
+chown -R mailpile:mailpile /mailpile-data/.gnupg
+
 su - mailpile <<EOF
 /Mailpile/mp --www=0.0.0.0:33411/mailpile/${CLOUDFLEET_USERNAME} --wait
 EOF
